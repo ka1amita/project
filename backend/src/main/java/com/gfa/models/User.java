e0116784e6e9bf7ba6930098f1a1354c2d952cfd
Matěj Kala <kalamatej@gmail.com>
2023-09-12 20:44:47 +0200

    Add User @Entity

---
+++ backend/src/main/java/com/gfa/models/User.java
@@ -0,0 +1,78 @@
+package com.gfa.models;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
+
+  String name;
+
+  String username;
+
+  String password;
+
+  @ManyToMany(fetch = FetchType.EAGER)
+  Collection<Role> roles = new ArrayList<>();
+
+  public User() {}
+
+  public User(Long id, String name, String username, String password, Collection<Role> roles) {
+    this.id = id;
+    this.name = name;
+    this.username = username;
+    this.password = password;
+    this.roles = roles;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Collection<Role> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(Collection<Role> roles) {
+    this.roles = roles;
+  }
+}
