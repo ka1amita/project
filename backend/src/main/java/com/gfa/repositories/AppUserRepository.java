2f9de56220bad01ca41595df5f1ed0b9c75dad6e
Daniel Kuhne <mrkuhne@icloud.com>
2023-09-14 00:12:40 +0200

    Changed to avoid merging conflicts.

--- backend/src/main/java/com/gfa/repositories/AppUserRepository.java
+++ backend/src/main/java/com/gfa/repositories/AppUserRepository.java
@@ -7,9 +7,7 @@ import java.util.Optional;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByUsernameContains(String username);
-
     Optional<AppUser> findByEmailContains(String email);
-
     Optional<AppUser> findByEmailContainsAndUsernameContains(String email, String username);
 
 }
