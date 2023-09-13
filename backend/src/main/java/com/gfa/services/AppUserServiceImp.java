374d64173b0ac2892225e9c296737c1d8d4d4524
Daniel Kuhne <mrkuhne@icloud.com>
2023-09-13 19:05:36 +0200

    Implemented the userLogin service method in the AppUserServiceImp class.

--- backend/src/main/java/com/gfa/services/AppUserServiceImp.java
+++ backend/src/main/java/com/gfa/services/AppUserServiceImp.java
@@ -1,4 +1,38 @@
 package com.gfa.services;
 
-public class AppUserServiceImp {
+import com.gfa.dtos.requestdtos.LoginRequestDto;
+import com.gfa.dtos.responsedtos.LoginResponseDto;
+import com.gfa.models.AppUser;
+import com.gfa.repositories.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AppUserServiceImp implements AppUserService {
+
+    private final AppUserRepository appUserRepository;
+
+    @Autowired
+    public AppUserServiceImp(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Override
+    public LoginResponseDto userLogin(Optional<LoginRequestDto> loginRequestDto) {
+        LoginRequestDto payload = loginRequestDto.orElseThrow(() -> new NullPointerException("Input body was not received."));
+        if(payload.getLoginInput() == null || payload.getLoginInput().isEmpty()) {
+            throw new IllegalArgumentException("Plesae provide a name or an email address.");
+        }
+        if(payload.getPassword() == null || payload.getPassword().isEmpty()) {
+            throw new IllegalArgumentException("Please provide a password.");
+        }
+        AppUser appUser = appUserRepository.findByUsernameOrEmail(payload.getLoginInput(), payload.getLoginInput())
+                .orElseThrow(() -> new NullPointerException("The user can not be found in the database."));
+        if (!appUser.getPassword().equals(payload.getPassword()))
+            throw new IllegalArgumentException("The password is incorrect.");
+        return new LoginResponseDto("Demo token");
+    }
 }
+
