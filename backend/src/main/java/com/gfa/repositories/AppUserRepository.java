ed15e718d77c57a901448ba897ca5b8fc331143b
Daniel Kuhne <mrkuhne@icloud.com>
2023-09-14 00:10:52 +0200

    Changed to avoid merging conflicts.

--- backend/src/main/java/com/gfa/repositories/AppUserRepository.java
+++ backend/src/main/java/com/gfa/repositories/AppUserRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-
-
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByUsernameContains(String username);
+
     Optional<AppUser> findByEmailContains(String email);
+
     Optional<AppUser> findByEmailContainsAndUsernameContains(String email, String username);
+
     AppUser findByUsername(String username);
 }
 
