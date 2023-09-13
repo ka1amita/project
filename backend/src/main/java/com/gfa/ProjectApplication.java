2df1ecaa14b8dc7378c07a3965672945674db638
Daniel Kuhne <mrkuhne@icloud.com>
2023-09-13 22:16:11 +0200

    Updated application properties to fit testing purposes. Added JUnit dependencies to build.gradle.

--- backend/src/main/java/com/gfa/ProjectApplication.java
+++ backend/src/main/java/com/gfa/ProjectApplication.java
@@ -14,29 +14,29 @@ import java.util.ArrayList;
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ProjectApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(ProjectApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApplication.class, args);
+    }
 
-  @Bean
-  CommandLineRunner run(AppUserService appUserService) {
-    return args -> {
-      appUserService.saveRole(new Role("ROLE_USER"));
-      appUserService.saveRole(new Role("ROLE_MANAGER"));
-      appUserService.saveRole(new Role("ROLE_ADMIN"));
-      appUserService.saveRole(new Role("ROLE_SUPER_ADMIN"));
+    @Bean
+    CommandLineRunner run(AppUserService appUserService) {
+        return args -> {
+            appUserService.saveRole(new Role("ROLE_USER"));
+            appUserService.saveRole(new Role("ROLE_MANAGER"));
+            appUserService.saveRole(new Role("ROLE_ADMIN"));
+            appUserService.saveRole(new Role("ROLE_SUPER_ADMIN"));
 
-      appUserService.saveAppUser(new AppUser("John Doe", "1234", "example1@mail.com", new ArrayList<>()));
-      appUserService.saveAppUser(new AppUser("Will Doe", "1234", "example2@mail.com", new ArrayList<>()));
-      appUserService.saveAppUser(new AppUser("Arnold Doe", "1234", "example3@mail.com", new ArrayList<>()));
-      appUserService.saveAppUser(new AppUser("Jim Doe", "1234", "example4@mail.com", new ArrayList<>()));
+            appUserService.saveAppUser(new AppUser("John Doe", "1234", "example1@mail.com", new ArrayList<>()));
+            appUserService.saveAppUser(new AppUser("Will Doe", "1234", "example2@mail.com", new ArrayList<>()));
+            appUserService.saveAppUser(new AppUser("Arnold Doe", "1234", "example3@mail.com", new ArrayList<>()));
+            appUserService.saveAppUser(new AppUser("Jim Doe", "1234", "example4@mail.com", new ArrayList<>()));
 
-      appUserService.addRoleToAppUser("John Doe", "ROLE_USER");
-      appUserService.addRoleToAppUser("Will Doe", "ROLE_MANAGER");
-      appUserService.addRoleToAppUser("Jim Doe", "ROLE_ADMIN");
-      appUserService.addRoleToAppUser("Arnold Doe", "ROLE_ADMIN");
-      appUserService.addRoleToAppUser("Arnold Doe", "ROLE_SUPER_ADMIN");
-      appUserService.addRoleToAppUser("Arnold Doe", "ROLE_MANAGER");
-    };
-  }
+            appUserService.addRoleToAppUser("John Doe", "ROLE_USER");
+            appUserService.addRoleToAppUser("Will Doe", "ROLE_MANAGER");
+            appUserService.addRoleToAppUser("Jim Doe", "ROLE_ADMIN");
+            appUserService.addRoleToAppUser("Arnold Doe", "ROLE_ADMIN");
+            appUserService.addRoleToAppUser("Arnold Doe", "ROLE_SUPER_ADMIN");
+            appUserService.addRoleToAppUser("Arnold Doe", "ROLE_MANAGER");
+        };
+    }
 }
