1df787ef58722ff0da554dbd9208f034b21d1d0f
Chi Lan Doan <chiland04@gmail.com>
2023-09-14 17:11:23 +0200

    Resolve conflict in ActivationCodeRepository

--- backend/src/main/java/com/gfa/repositories/ActivationCodeRepository.java
+++ backend/src/main/java/com/gfa/repositories/ActivationCodeRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ActivationCodeRepository extends JpaRepository<ActivationCode, Long> {
-
     Optional<ActivationCode> findByActivationCodeContains(String activationCode);
-
 }
