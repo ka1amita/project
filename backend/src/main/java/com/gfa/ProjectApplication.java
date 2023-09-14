9fb50c8507a616eb2f2c72318b19be127f7842a9
Matěj Kala <kalamatej@gmail.com>
2023-09-14 23:57:15 +0200

    Remove comments from ProjectApplication

--- backend/src/main/java/com/gfa/ProjectApplication.java
+++ backend/src/main/java/com/gfa/ProjectApplication.java
@@ -2,7 +2,6 @@ package com.gfa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication()
 public class ProjectApplication {
@@ -10,26 +9,4 @@ public class ProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
     }
-
-//    @Bean
-//    CommandLineRunner run(LoginUserService appUserService) {
-//        return args -> {
-//            appUserService.saveRole(new Role("ROLE_USER"));
-//            appUserService.saveRole(new Role("ROLE_MANAGER"));
-//            appUserService.saveRole(new Role("ROLE_ADMIN"));
-//            appUserService.saveRole(new Role("ROLE_SUPER_ADMIN"));
-//
-//            appUserService.saveUser(new AppUser("John Doe", "1234", "example1@mail.com", new ArrayList<>()));
-//            appUserService.saveUser(new AppUser("Will Doe", "1234", "example2@mail.com", new ArrayList<>()));
-//            appUserService.saveUser(new AppUser("Arnold Doe", "1234", "example3@mail.com", new ArrayList<>()));
-//            appUserService.saveUser(new AppUser("Jim Doe", "1234", "example4@mail.com", new ArrayList<>()));
-//
-//            appUserService.addRoleToUser("John Doe", "ROLE_USER");
-//            appUserService.addRoleToUser("Will Doe", "ROLE_MANAGER");
-//            appUserService.addRoleToUser("Jim Doe", "ROLE_ADMIN");
-//            appUserService.addRoleToUser("Arnold Doe", "ROLE_ADMIN");
-//            appUserService.addRoleToUser("Arnold Doe", "ROLE_SUPER_ADMIN");
-//            appUserService.addRoleToUser("Arnold Doe", "ROLE_MANAGER");
-//        };
-//    }
 }
