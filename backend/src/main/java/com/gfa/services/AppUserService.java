20d2c2144fcf7dea202b46a34dc41bff8592d280
Chi Lan Doan <chiland04@gmail.com>
2023-09-14 16:37:06 +0200

    Resolve conflict in AppUserService. NOTE: uses classes that are not present in my branch

--- backend/src/main/java/com/gfa/services/AppUserService.java
+++ backend/src/main/java/com/gfa/services/AppUserService.java
@@ -4,16 +4,23 @@ package com.gfa.services;
 import com.gfa.dtos.responsedtos.RegisterRequestDTO;
 import com.gfa.models.AppUser;
 
+import com.gfa.dtos.requestdtos.LoginRequestDTO;
+import com.gfa.dtos.requestdtos.PasswordResetRequestDTO;
+import com.gfa.dtos.requestdtos.PasswordResetWithCodeRequestDTO;
+import com.gfa.dtos.responsedtos.LoginResponseDTO;
+import com.gfa.dtos.responsedtos.ResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 public interface AppUserService {
 
     AppUser registerUser(RegisterRequestDTO request);
-
-    boolean isUsernameUnique(String username);
-
-    boolean isEmailUnique(String email);
-
     void activateAccount (String activationCode);
+    ResponseEntity<ResponseDTO> reset(PasswordResetRequestDTO passwordResetRequestDTO);
 
+    ResponseEntity<ResponseDTO> resetWithCode(PasswordResetWithCodeRequestDTO passwordResetWithCodeRequestDTO, String resetCode);
 
+    LoginResponseDTO userLogin(Optional<LoginRequestDTO> loginRequestDto);
 }
