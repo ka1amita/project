049754fe25d82884217b301ef2e87032029e95a2
Daniel Gambino <gdani14@gmail.com>
2023-09-14 15:17:29 +0200

    Tiny refactoring for testing

--- backend/src/main/java/com/gfa/ProjectApplication.java
+++ backend/src/main/java/com/gfa/ProjectApplication.java
@@ -1,8 +1,18 @@
 package com.gfa;
 
+import com.gfa.models.ActivationCode;
+import com.gfa.models.AppUser;
+import com.gfa.models.Role;
+import com.gfa.repositories.ActivationCodeRepository;
+import com.gfa.repositories.AppUserRepository;
+import com.gfa.services.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication()
 public class ProjectApplication {
@@ -12,24 +22,31 @@ public class ProjectApplication {
     }
 
 //    @Bean
-//    CommandLineRunner run(LoginUserService appUserService) {
+//    CommandLineRunner run(UserService userService, ActivationCodeRepository activationCodeRepository) {
 //        return args -> {
-//            appUserService.saveRole(new Role("ROLE_USER"));
-//            appUserService.saveRole(new Role("ROLE_MANAGER"));
-//            appUserService.saveRole(new Role("ROLE_ADMIN"));
-//            appUserService.saveRole(new Role("ROLE_SUPER_ADMIN"));
+//            userService.saveRole(new Role("ROLE_USER"));
+//            userService.saveRole(new Role("ROLE_MANAGER"));
+//            userService.saveRole(new Role("ROLE_ADMIN"));
+//            userService.saveRole(new Role("ROLE_SUPER_ADMIN"));
 //
-//            appUserService.saveUser(new AppUser("John Doe", "1234", "example1@mail.com", new ArrayList<>()));
-//            appUserService.saveUser(new AppUser("Will Doe", "1234", "example2@mail.com", new ArrayList<>()));
-//            appUserService.saveUser(new AppUser("Arnold Doe", "1234", "example3@mail.com", new ArrayList<>()));
-//            appUserService.saveUser(new AppUser("Jim Doe", "1234", "example4@mail.com", new ArrayList<>()));
+//            AppUser appUser1 = new AppUser("John Doe", "1234", "example1@mail.com", new ArrayList<>());
+//            AppUser appUser2 = new AppUser("Will Doe", "1234", "example2@mail.com", new ArrayList<>());
+//            AppUser appUser3 = new AppUser("Arnold Doe", "1234", "example3@mail.com", new ArrayList<>());
+//            AppUser appUser4 = new AppUser("Jim Doe", "1234", "example4@mail.com", new ArrayList<>());
 //
-//            appUserService.addRoleToUser("John Doe", "ROLE_USER");
-//            appUserService.addRoleToUser("Will Doe", "ROLE_MANAGER");
-//            appUserService.addRoleToUser("Jim Doe", "ROLE_ADMIN");
-//            appUserService.addRoleToUser("Arnold Doe", "ROLE_ADMIN");
-//            appUserService.addRoleToUser("Arnold Doe", "ROLE_SUPER_ADMIN");
-//            appUserService.addRoleToUser("Arnold Doe", "ROLE_MANAGER");
+//            userService.saveUser(appUser1);
+//            userService.saveUser(appUser2);
+//            userService.saveUser(appUser3);
+//            userService.saveUser(appUser4);
+//
+//            userService.addRoleToUser("John Doe", "ROLE_USER");
+//            userService.addRoleToUser("Will Doe", "ROLE_MANAGER");
+//            userService.addRoleToUser("Jim Doe", "ROLE_ADMIN");
+//            userService.addRoleToUser("Arnold Doe", "ROLE_ADMIN");
+//            userService.addRoleToUser("Arnold Doe", "ROLE_SUPER_ADMIN");
+//            userService.addRoleToUser("Arnold Doe", "ROLE_MANAGER");
+//
+//            activationCodeRepository.save(new ActivationCode("ctrauzhrdquulnctfhyrtiaztmrsnniwxggfoeurcbyctvhd", appUser2));
 //        };
 //    }
 }
