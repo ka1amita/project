62ad307d1971f3a57bf4fd1ea03e9ce464133022
Chi Lan Doan <chiland04@gmail.com>
2023-09-14 16:53:31 +0200

    Resolve conflicts in Role

--- backend/src/main/java/com/gfa/models/Role.java
+++ backend/src/main/java/com/gfa/models/Role.java
@@ -1,23 +1,35 @@
 package com.gfa.models;
 
-
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.jetbrains.annotations.NotNull;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "user_role")
+@Table(name = "roles")
 public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, updatable = false)
     private Long id;
+    @Column(unique = true)
+    @NotNull
+    private String name;
     @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    @JsonBackReference
     private List<AppUser> appUsers = new ArrayList<>();
 
     public Role() {
+    }
+
+    public Role(Long id, @NotNull String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public Long getId() {
@@ -27,12 +39,22 @@ public class Role {
     public void setId(Long id) {
         this.id = id;
     }
+    @NotNull
+    public String getName() {
+        return name;
+    }
 
-    public List<AppUser> getUsers() {
+    public void setName(@NotNull String name) {
+        this.name = name;
+    }
+
+    public List<AppUser> getAppUsers() {
         return appUsers;
     }
 
-    public void setUsers(List<AppUser> appUsers) {
-        this.appUsers = appUsers;
+    public void setAppUsers(List<AppUser> appUsers) {
+        {
+            this.appUsers = appUsers;
+        }
     }
 }
