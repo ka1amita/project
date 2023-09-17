f8b4c6be0ca7b1b857d1c131fa1b3b468a2ddd72
Chi Lan Doan <chiland04@gmail.com>
2023-09-17 15:07:11 +0200

    Modified ActivationCodeRepository

--- backend/src/main/java/com/gfa/repositories/ActivationCodeRepository.java
+++ backend/src/main/java/com/gfa/repositories/ActivationCodeRepository.java
@@ -1,10 +1,13 @@
 package com.gfa.repositories;
 
 import com.gfa.models.ActivationCode;
+import com.gfa.models.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ActivationCodeRepository extends JpaRepository<ActivationCode, Long> {
     Optional<ActivationCode> findByActivationCodeContains(String activationCode);
+
+    Optional<ActivationCode> findByAppUser(AppUser user);
 }
