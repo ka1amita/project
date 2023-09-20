a3b7b1d59b1d4af85755484aff5382c7dc8e1e21
Daniel Gambino <gdani14@gmail.com>
2023-09-20 14:17:00 +0200

    Small changes (relying more on Spring Security)

--- backend/src/main/java/com/gfa/controllers/DashboardController.java
+++ backend/src/main/java/com/gfa/controllers/DashboardController.java
@@ -1,8 +1,6 @@
 package com.gfa.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/dashboard")
 public class DashboardController {
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
     @GetMapping({"", "/"})
     public ResponseEntity<?> index() {
-        if (authentication.isAuthenticated()) {
-            return ResponseEntity.ok().build();
-        } else {
-            return ResponseEntity.status(401).build();
-        }
+        return ResponseEntity.ok().build();
     }
 }
