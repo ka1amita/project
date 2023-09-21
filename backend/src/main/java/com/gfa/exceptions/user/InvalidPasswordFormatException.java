6a19d4d75c93a05d8f2a2b7bfecce00d1ca9b184
Chi Lan Doan <chiland04@gmail.com>
2023-09-21 23:24:50 +0200

    Delete unused constructors

--- backend/src/main/java/com/gfa/exceptions/user/InvalidPasswordFormatException.java
+++ backend/src/main/java/com/gfa/exceptions/user/InvalidPasswordFormatException.java
@@ -3,14 +3,8 @@ package com.gfa.exceptions.user;
 import com.gfa.exceptions.AppRuntimeException;
 
 public class InvalidPasswordFormatException extends AppRuntimeException {
-    public InvalidPasswordFormatException() {
-    }
-
     public InvalidPasswordFormatException(String message) {
         super(message);
     }
 
-    public InvalidPasswordFormatException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
