cab6620de98b683edd48403425db7a9bf3e6b4ef
Daniel Kuhne <mrkuhne@icloud.com>
2023-09-21 13:41:27 +0200

    Changed the "/user/users/**" endpoint to "/users/**" and added the Delete user endpoint

---
+++ backend/src/main/java/com/gfa/controllers/UserRestController.java
@@ -0,0 +1,26 @@
+package com.gfa.controllers;
+
+import com.gfa.models.AppUser;
+import com.gfa.services.AppUserService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserRestController {
+
+  public final AppUserService appUserService;
+
+  @Autowired
+  public UserRestController(AppUserService appUserService) {
+    this.appUserService = appUserService;
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> destroy(@PathVariable Long id) {
+    appUserService.removeAppUser(id);
+    return ResponseEntity.status(201).build();
+  }
+}
