c176f81fc975edabd36b7c73e600fbd92510c2b7
Chi Lan Doan <chiland04@gmail.com>
2023-09-21 23:11:32 +0200

    Delete unused constructors

--- backend/src/main/java/com/gfa/exceptions/role/RoleNotFoundException.java
+++ backend/src/main/java/com/gfa/exceptions/role/RoleNotFoundException.java
@@ -3,14 +3,7 @@ package com.gfa.exceptions.role;
 import com.gfa.exceptions.AppRuntimeException;
 
 public class RoleNotFoundException extends AppRuntimeException {
-    public RoleNotFoundException() {
-    }
-
-    public RoleNotFoundException(String message) {
+     public RoleNotFoundException(String message) {
         super(message);
-    }
-
-    public RoleNotFoundException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
--- backend/src/main/java/com/gfa/exceptions/user/UserNotFoundException.java
+++ backend/src/main/java/com/gfa/exceptions/user/UserNotFoundException.java
@@ -3,14 +3,8 @@ package com.gfa.exceptions.user;
 import com.gfa.exceptions.AppRuntimeException;
 
 public class UserNotFoundException extends AppRuntimeException {
-    public UserNotFoundException() {
-    }
-
     public UserNotFoundException(String message) {
         super(message);
     }
 
-    public UserNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
