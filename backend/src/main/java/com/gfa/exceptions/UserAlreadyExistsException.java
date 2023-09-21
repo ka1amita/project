fd7a6e63f97dfa3c7010728a5faf3b2d862b88ba
Chi Lan Doan <chiland04@gmail.com>
2023-09-21 23:18:25 +0200

    Deleted non-needed classes

--- backend/src/main/java/com/gfa/exceptions/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.gfa.exceptions;
-
-public class UserAlreadyExistsException extends RuntimeException{
-    public UserAlreadyExistsException(String message){
-        super(message);
-    }
-}
