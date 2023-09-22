6c80306cd4cc9b9e019fd3a7bba799d64b854304
Matěj Kala <kalamatej@gmail.com>
2023-09-22 09:56:29 +0200

    Add pagination page and size RequestParam

--- backend/src/main/java/com/gfa/controllers/UserRestController.java
+++ backend/src/main/java/com/gfa/controllers/UserRestController.java
@@ -4,13 +4,21 @@ import static com.gfa.utils.Endpoint.USERS;
 import static org.springframework.http.HttpStatus.OK;
 
 import com.gfa.services.AppUserService;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(USERS)
 public class UserRestController {
+  @Value("${pagination.size.default}")
+  private Integer defaultSize;
+  @Value("${pagination.size.max}")
+  private Integer maxSize;
 
   public final AppUserService appUserService;
 
@@ -24,11 +32,21 @@ public class UserRestController {
     appUserService.removeAppUser(id);
     return ResponseEntity.status(201).build();
   }
-  // Pagination Example, check https://www.baeldung.com/spring-data-jpa-pagination-sorting
-  @DeleteMapping("/list?page=1")
-  public ResponseEntity<?> destroy(@RequestParam Integer page, @RequestParam Integer entries) {
+/**
+ *  Pagination Example, check https://www.baeldung.com/spring-data-jpa-pagination-sorting
+ * 1. Repo has to extend PagingAndSortingRepository (note that JpaRepository does so already)
+ * 2.
+*/
+  @PostMapping("/list")
+  public ResponseEntity<?> getPagedActiveNonDeletedUsers(@RequestParam(required = false,name = "page") Optional<Integer> optPage, @RequestParam(required = false,name = "entries")
+  Optional<Integer> optSize) {
+    Integer page = optPage.orElse(1) - 1; // conversion to zero-based index
+    Integer size = (!optSize.isPresent() || optSize.get() > maxSize) ? optSize.get() : defaultSize;
 
-    return ResponseEntity.status(OK).build();
+    Pageable pageRequest = PageRequest.of(0, 1);
+
+
+    return ResponseEntity.status(OK).body("page: " + page + ", size: " + size);
   }
 
 
