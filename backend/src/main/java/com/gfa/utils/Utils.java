1d08e2aa4fbece4df4c0788197d95170f887c17d
Chi Lan Doan <chiland04@gmail.com>
2023-09-22 14:38:25 +0200

    Update regex to include all special characters

--- backend/src/main/java/com/gfa/utils/Utils.java
+++ backend/src/main/java/com/gfa/utils/Utils.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.util.stream.Collectors;
 
 public class Utils {
-    private static final String UserPasswordPattern = "^(?=.[a-z])(?=.[A-Z])(?=.\\d)(?=.[~!@#$%^&*()\\\\-_=+\\\\[\\\\]{}\\\\|;:'\",.<>/?]).[A-Za-z\\\\d~!@#$%^&*()\\-_=+\\[\\]{}\\|;:'\",.<>/?]{8,}$";
+    private static final String UserPasswordPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!\"#$%&'()*+,-./:;<=>?@\\[\\]\\\\^_`{|}~]).{8,}$";
     private static final String ActivationCodeGenerationSample = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     public static String GenerateActivationCode(Integer size) {
