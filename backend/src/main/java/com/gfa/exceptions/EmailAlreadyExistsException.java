e846b70365c72c4ad073301f54b81f144029163f
Daniel Kuhne <mrkuhne@icloud.com>
2023-09-22 19:23:57 +0200

    Updated the exception

--- backend/src/main/java/com/gfa/exceptions/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.gfa.exceptions;
-
-public class EmailAlreadyExistsException extends AppRuntimeException{
-    public EmailAlreadyExistsException(String message) {
-        super(message);
-    }
-}
