4ad21b5e9346fdd9b29a937fcc1b643d8fa33e6f
Chi Lan Doan <chiland04@gmail.com>
2023-09-25 17:59:36 +0200

    Add code to handle localization in every endpoint

--- backend/src/main/java/com/gfa/filters/CustomAuthorizationFilter.java
+++ backend/src/main/java/com/gfa/filters/CustomAuthorizationFilter.java
@@ -5,14 +5,21 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gfa.dtos.responsedtos.ErrorResponseDTO;
+import com.gfa.models.AppUser;
+import com.gfa.repositories.AppUserRepository;
 import com.gfa.services.TokenService;
 import com.gfa.utils.Endpoint;
+
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Optional;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,59 +29,97 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
-  private final TokenService tokenService;
+    private final TokenService tokenService;
+    //add
+    private final AppUserRepository appUserRepository;
 
-  @Autowired
-  public CustomAuthorizationFilter(TokenService tokenService) {
-    this.tokenService = tokenService;
-  }
 
-  private static void handleException(HttpServletResponse response, Exception e)
-      throws IOException {
-    String message = e.getMessage();
-    response.setHeader("Error", message);
-    response.setStatus(UNAUTHORIZED.value());
-    response.setContentType(APPLICATION_JSON_VALUE);
-    new ObjectMapper().writeValue(response.getOutputStream(),
-                                  new ErrorResponseDTO(message));
-  }
-
-  @Override
-  protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,
-                                  @NonNull FilterChain filterChain) throws ServletException,
-                                                                            IOException {
-    if (request.getServletPath()
-               .equals(Endpoint.HELLO_WORLD.getValue()) ||
-        request.getServletPath()
-               .equals(Endpoint.REGISTER.getValue()) ||
-        request.getServletPath()
-               .equals(Endpoint.LOGIN.getValue()) ||
-        request.getServletPath()
-               .equals(Endpoint.REFRESH_TOKEN.getValue()) ||
-        request.getServletPath()
-               .startsWith(Endpoint.RESET_PASSWORD.getValue()) ||
-        request.getServletPath()
-               .equals(Endpoint.RESEND_VERIFICATION_EMAIL.getValue()) ||
-        request.getServletPath()
-               .startsWith(Endpoint.VERIFY_EMAIL_WITH_TOKEN.getValue()) ||
-        request.getServletPath()
-               .startsWith(Endpoint.CONFIRM_WITH_CODE.getValue())
-    ) {
-
-      filterChain.doFilter(request, response);
-    } else {
-      try {
-        Authentication authentication =
-            tokenService.getAuthentication(tokenService.mapToDto(request));
-
-        SecurityContextHolder.getContext()
-                             .setAuthentication(authentication);
-
-      } catch (Exception e) {
-        handleException(response, e);
-      } finally {
-        filterChain.doFilter(request, response);
-      }
+    @Autowired
+    public CustomAuthorizationFilter(TokenService tokenService, AppUserRepository appUserRepository) {
+        this.tokenService = tokenService;
+        this.appUserRepository = appUserRepository;
     }
-  }
+
+    private static void handleException(HttpServletResponse response, Exception e)
+            throws IOException {
+        String message = e.getMessage();
+        response.setHeader("Error", message);
+        response.setStatus(UNAUTHORIZED.value());
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(),
+                new ErrorResponseDTO(message));
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException,
+            IOException {
+        System.err.println("inside filter");
+        if (request.getServletPath().startsWith(Endpoint.CONFIRM_WITH_CODE.getValue())) {
+            System.err.println("insideactivateendpoint");
+            String[] subPath = request.getServletPath().split("/");
+            String activationCode = subPath[subPath.length - 1];
+            Optional<AppUser> userWithCode = appUserRepository.findAppUserByActivationCode(activationCode);
+            if (userWithCode.isPresent()) {
+                setPreferredLanguageForAppUser(userWithCode);
+            }
+        }
+        if (request.getServletPath()
+                .equals(Endpoint.HELLO_WORLD.getValue()) ||
+                request.getServletPath()
+                        .equals(Endpoint.REGISTER.getValue()) ||
+                request.getServletPath()
+                        .equals(Endpoint.LOGIN.getValue()) ||
+                request.getServletPath()
+                        .equals(Endpoint.REFRESH_TOKEN.getValue()) ||
+                request.getServletPath()
+                        .startsWith(Endpoint.RESET_PASSWORD.getValue()) ||
+                request.getServletPath()
+                        .equals(Endpoint.RESEND_VERIFICATION_EMAIL.getValue()) ||
+                request.getServletPath()
+                        .startsWith(Endpoint.VERIFY_EMAIL_WITH_TOKEN.getValue()) ||
+                request.getServletPath()
+                        .startsWith(Endpoint.CONFIRM_WITH_CODE.getValue())||
+                request.getServletPath()
+                .startsWith(Endpoint.STRINGS.getValue())
+        ) {
+            filterChain.doFilter(request, response);
+        } else {
+            try {
+                Authentication authentication =
+                        tokenService.getAuthentication(tokenService.mapToDto(request));
+//---------------
+                if (authentication != null) {
+                    System.err.println("auth is not null");
+                    AppUser user = (AppUser) authentication.getPrincipal();
+                    setPreferredLanguage(user.getUsername());
+                    SecurityContextHolder.getContext()
+                            .setAuthentication(authentication);
+                }
+//---------------
+            } catch (Exception e) {
+                handleException(response, e);
+            } finally {
+                filterChain.doFilter(request, response);
+            }
+        }
+    }
+
+    private void setPreferredLanguage(String username) {
+        Optional<AppUser> appUser = appUserRepository.findByUsername(username);
+        if (appUser.isPresent()) {
+            String preferredLang = appUser.get().getPreferredLanguage();
+            System.err.println("setting preffered language");
+            LocaleContextHolder.setLocale(new Locale(preferredLang));
+        }
+    }
+
+    private void setPreferredLanguageForAppUser(Optional<AppUser> appUser) {
+        if (appUser.isPresent()) {
+            String preferredLang = appUser.get().getPreferredLanguage();
+            System.err.println("setting preffered language2");
+            System.err.println(preferredLang);
+            LocaleContextHolder.setLocale(new Locale(preferredLang));
+        }
+    }
 }
