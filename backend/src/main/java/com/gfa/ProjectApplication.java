2ecf1681fe7ec97dac64c32d2c13d7111855166f
Matěj Kala <kalamatej@gmail.com>
2023-09-25 21:46:27 +0200

    Prevent automatic migration on App start-up

--- backend/src/main/java/com/gfa/ProjectApplication.java
+++ backend/src/main/java/com/gfa/ProjectApplication.java
@@ -1,13 +1,20 @@
 package com.gfa;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication()
 public class ProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
+    }
+
+    @Bean
+    FlywayMigrationStrategy flywayMigrationStrategy() {
+        // prevents automatic migration on App start-up
+        return args -> {};
     }
 }
