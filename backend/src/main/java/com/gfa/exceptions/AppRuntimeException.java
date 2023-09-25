4769d6822773c4d2d6771c00844a8814061add0f
Chi Lan Doan <chiland04@gmail.com>
2023-09-25 17:53:29 +0200

    Delete unused constructors

--- backend/src/main/java/com/gfa/exceptions/AppRuntimeException.java
+++ backend/src/main/java/com/gfa/exceptions/AppRuntimeException.java
@@ -1,15 +1,8 @@
 package com.gfa.exceptions;
 
 public class AppRuntimeException extends RuntimeException {
-
-    public AppRuntimeException() {
-    }
-
     public AppRuntimeException(String message) {
         super(message);
     }
 
-    public AppRuntimeException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
