d713daf3fd413ef16b780ec23bc2f8bc5294a5c7
Daniel Gambino <gdani14@gmail.com>
2023-09-25 16:55:01 +0200

    Authentication now uses Axios and JWT

--- frontend/src/api/authentication.js
+++ frontend/src/api/authentication.js
@@ -1,41 +1,27 @@
 import "api/endpoints"
-import React from "react";
-import { useJwt } from "react-jwt";
+import {postRequest} from "/api/requesthandler"
+import React, {useState} from "react";
+import {useJwt} from "react-jwt";
 
+const axios = require('axios');
 
-
-// TODO: Maybe use Axiom for requests?
 export const login = async (credentials) => {
-    try {
-        const response = await fetch(`${API_BASE_URL + API_LOGIN_URL}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(credentials),
-        });
-        const data = await response.json();
-        const { decodedToken, isExpired } = useJwt(data.token);
-        // TODO: Set condition for login...
-
-        // TODO: Which one to use: Local storage (persists)
-        //  OR Session storage (gets destroyed when user closes browser)
-        //   OR State management (needs Redux or Context API)
-        // TODO: THIS NEEDS FURTHER RESEARCH...
-        // localStorage.setItem('token', yourToken);
-        // const token = localStorage.getItem('token');
-        // sessionStorage.setItem('token', yourToken);
-        // const token = sessionStorage.getItem('token');
-        // dispatch({ type: 'SET_TOKEN', payload: yourToken });
-        // const token = useSelector(state => state.token);
-        return data;
-    } catch (error) {
-        console.error('Login failed:', error);
-        throw error;
-    }
+    await postRequest(API_LOGIN_URL, {
+        username: credentials.username,
+        password: credentials.password
+    }, (response) => {
+        const {decodedToken, isExpired} = useJwt(response.token);
+        if (!isExpired) {
+            localStorage.setItem('token', JSON.stringify(decodedToken));
+            axios.defaults.headers.common["Authorization"] = `Bearer ${decodedToken}`;
+        } else {
+            logout();
+        }
+    });
 };
 
 export const logout = async () => {
-    setIsLoggedIn(false);
+    localStorage.removeItem('token');
+    delete axios.defaults.headers.common["Authorization"];
 };
 
