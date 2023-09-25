232cb1cebadbbde6180f1ec87e04af79c5830ce9
Matěj Kala <kalamatej@gmail.com>
2023-09-25 10:27:13 +0200

    Add Flyway to ProjectApplication
    Linked to .properties and scripts in resources/db/migration

--- backend/src/main/java/com/gfa/ProjectApplication.java
+++ backend/src/main/java/com/gfa/ProjectApplication.java
@@ -1,5 +1,6 @@
 package com.gfa;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,10 @@ public class ProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
+        // Create the Flyway instance and point it to the database
+        Flyway flyway = Flyway.configure().dataSource("jdbc:mysql://localhost:3306/fox-dining?serverTimezone=UTC", "root", "password").load();
+flyway.getConfiguration();
+        // Start the migration
+        flyway.migrate();
     }
 }
