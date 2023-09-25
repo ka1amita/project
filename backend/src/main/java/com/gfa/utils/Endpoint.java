4e8ad5785d02f3932a7a8a9370187d6aa2d90645
Chi Lan Doan <chiland04@gmail.com>
2023-09-25 18:02:01 +0200

    Add endpoint Strings to not require auth

--- backend/src/main/java/com/gfa/utils/Endpoint.java
+++ backend/src/main/java/com/gfa/utils/Endpoint.java
@@ -5,7 +5,8 @@ public enum Endpoint {
     REGISTER("/register"), CONFIRM_WITH_CODE("/confirm/"), LOGIN("/login"),
     RESET_PASSWORD("/reset"),
     VERIFY_EMAIL_WITH_TOKEN("/email/verify/"), RESEND_VERIFICATION_EMAIL("/email/verify/resend"),
-    DASHBOARD("/dashboard"), REFRESH_TOKEN("/token/refresh"), HELLO_WORLD("/hello");
+    DASHBOARD("/dashboard"), REFRESH_TOKEN("/token/refresh"), HELLO_WORLD("/hello"),
+    STRINGS("/strings");
     private final String value;
 
     Endpoint(String value) {
