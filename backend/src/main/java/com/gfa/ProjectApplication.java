93e5284a84c63cec7abfa7bd1d17e9989d704c0b
Matěj Kala <kalamatej@gmail.com>
2023-09-25 21:41:45 +0200

    Remove Flyway from ProjectApplication and add flyway{} to .properties
     Run it automatically with ./gradlew bootRun.

--- backend/src/main/java/com/gfa/ProjectApplication.java
+++ backend/src/main/java/com/gfa/ProjectApplication.java
@@ -9,10 +9,5 @@ public class ProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
-        // Create the Flyway instance and point it to the database
-        Flyway flyway = Flyway.configure().dataSource("jdbc:mysql://localhost:3306/fox-dining?serverTimezone=UTC", "root", "password").load();
-flyway.getConfiguration();
-        // Start the migration
-        flyway.migrate();
     }
 }
