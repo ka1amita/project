395a92be02cdbe43e2d7bfc7cb29c9b2bb1f3987
Daniel Kuhne <mrkuhne@icloud.com>
2023-09-26 17:53:25 +0200

    Created util functions for the TodoService (others use them as well)

--- backend/src/main/java/com/gfa/utils/Utils.java
+++ backend/src/main/java/com/gfa/utils/Utils.java
@@ -1,6 +1,9 @@
 package com.gfa.utils;
 
 import com.gfa.exceptions.user.InvalidIdException;
+import com.gfa.exceptions.user.MissingJSONBodyException;
+import org.springframework.security.core.Authentication;
+
 import java.util.Random;
 import java.util.stream.Collectors;
 
@@ -20,7 +23,18 @@ public class Utils {
         return password.matches(UserPasswordPattern);
     }
 
-    public static void IsUserIdValid(Long id) {
+    public static void IsProvidedIdValid(Long id) {
         if (id < 0) throw new InvalidIdException("Please provide a valid ID");
+    }
+    public static <T> void isJSONBodyPresent(T request) {
+        if (request == null) throw new MissingJSONBodyException("Please provide a JSON body");
+    }
+
+    public static Boolean hasAdminRole(Authentication authentication) {
+        return authentication.getAuthorities().stream()
+                .anyMatch(grantedAuthority -> "ADMIN".equals(grantedAuthority.getAuthority()));
+    }
+    public static Boolean isNotNullOrEmpty(String field) {
+        return field != null && !field.isEmpty();
     }
 }
