c77e0c1892b13a6e3bff49393ce6f5f4c62cd4d8
Daniel Kuhne <mrkuhne@icloud.com>
2023-09-26 17:51:24 +0200

    Added necessary query for user validation in todo service

--- backend/src/main/java/com/gfa/repositories/TodoRepository.java
+++ backend/src/main/java/com/gfa/repositories/TodoRepository.java
@@ -4,6 +4,10 @@ import com.gfa.models.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    List<Todo> findByAppUser_Username(String username);
 }
