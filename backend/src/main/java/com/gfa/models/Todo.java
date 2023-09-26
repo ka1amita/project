fc85e4748980b894ac3c6ef72266c88d5c7879d3
Daniel Kuhne <mrkuhne@icloud.com>
2023-09-26 17:50:46 +0200

    Finished the Todo entity

--- backend/src/main/java/com/gfa/models/Todo.java
+++ backend/src/main/java/com/gfa/models/Todo.java
@@ -1,19 +1,32 @@
 package com.gfa.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.hibernate.annotations.Where;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "todos")
+@Where(clause = "deleted=false")
 public class Todo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
     private Long id;
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false)
     private String description;
+    private boolean completed;
+    private boolean deleted;
+    @Column(columnDefinition = "TIMESTAMP")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime dueDate;
     @ManyToOne
+    @JsonBackReference
     private AppUser appUser;
 
     public Todo() {
@@ -56,6 +69,22 @@ public class Todo {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
     }
 
     public LocalDateTime getDueDate() {
