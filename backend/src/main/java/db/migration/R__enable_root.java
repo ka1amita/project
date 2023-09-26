62076f9783d9ab16e81faa518e07c5a856d42830
Matěj Kala <kalamatej@gmail.com>
2023-09-26 10:29:29 +0200

    Add bot enable disable functionality

--- backend/src/main/java/db/migration/R__enable_root.java
+++ backend/src/main/java/db/migration/R__enable_root.java
@@ -4,8 +4,9 @@ import java.sql.PreparedStatement;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
 
-// @Component
+@Component
 public class R__enable_root extends BaseJavaMigration {
 
     @Override
@@ -17,22 +18,30 @@ public class R__enable_root extends BaseJavaMigration {
         String password = context.getConfiguration()
                                  .getPlaceholders()
                                  .get("root.password");
-        if (isEnabled && !password.isEmpty()) {
-            String username = "'" + context.getConfiguration()
-                              .getPlaceholders()
-                                           .getOrDefault("root.username", "root") + "'";
-            String email = "'" + context.getConfiguration()
-                                        .getPlaceholders()
-                                        .getOrDefault("root.email", username + "@gfa.com") + "'";
-            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-            String encodedPassword = "'" + encoder.encode(password) + "'";
+        String username = "'" + context.getConfiguration()
+                                       .getPlaceholders()
+                                       .getOrDefault("root.username", "root") + "'";
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPassword = "'" + encoder.encode(password) + "'";
+
+        if (!isEnabled) {
             try (PreparedStatement statement = context
-                     .getConnection()
-                     .prepareStatement(
-                         "update app_users set active = true, password = " + encodedPassword +
-                         " where username = " + username + " ")) {
-            statement.execute();
-        }
+                .getConnection()
+                .prepareStatement(
+                    "update app_users set active = false where username = " + username)) {
+                statement.execute();
+
+            }
+        } else if (isEnabled && !password.isEmpty()) {
+            try (PreparedStatement statement = context
+                .getConnection()
+                .prepareStatement(
+                    "update app_users set active = true, password = " + encodedPassword +
+                    " where username = " + username)) {
+                statement.execute();
+            }
+        } else {
+            throw new IllegalArgumentException("Wrong root credentials");
         }
     }
 
