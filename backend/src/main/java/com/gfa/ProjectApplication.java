7feeb9870744b865096e8243a90ac9b6d7750222
Matěj Kala <kalamatej@gmail.com>
2023-10-01 15:46:31 +0200

    Clean up the flyway related code

--- backend/src/main/java/com/gfa/ProjectApplication.java
+++ backend/src/main/java/com/gfa/ProjectApplication.java
@@ -11,10 +11,4 @@ public class ProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
     }
-
-    // @Bean
-    // FlywayMigrationStrategy flywayMigrationStrategy() {
-    //     // prevents automatic migration on App start-up
-    //     return args -> {};
-    // }
 }
