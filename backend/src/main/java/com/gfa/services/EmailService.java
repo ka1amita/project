999b2b985a5163c77830e3efa61fd20b17d6f297
Daniel Gambino <gdani14@gmail.com>
2023-10-01 19:53:30 +0200

    Fixed email urls (made it parameterized, so that it can be changed) for backend /confirm and frontend /auth/reset-password

--- backend/src/main/java/com/gfa/services/EmailService.java
+++ backend/src/main/java/com/gfa/services/EmailService.java
@@ -5,7 +5,7 @@ import javax.mail.MessagingException;
 public interface EmailService {
     void sendSimpleMessage(String to, String subject, String text);
 
-    void sendMimeMessage(String to, String subject, String messageText, String code) throws MessagingException;
+    void sendMimeMessage(String to, String subject, String messageText, String code, String url) throws MessagingException;
 
     void registerConfirmationEmail(String to, String username, String code) throws MessagingException;
 
