3961deb58c1f046f5d1d0e32c87cd9b2088418e7
Matěj Kala <kalamatej@gmail.com>
2023-10-26 13:43:17 +0200

    Corrected CustomAuthenticationFilterTest

--- backend/src/test/java/com/gfa/services/CustomAuthenticationFilterTest.java
+++ backend/src/test/java/com/gfa/services/CustomAuthenticationFilterTest.java
@@ -1,14 +1,21 @@
 package com.gfa.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gfa.filters.CustomAuthenticationFilter;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockMultipartHttpServletRequest;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -16,10 +23,16 @@ import org.springframework.security.core.AuthenticationException;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class CustomAuthenticationFilterTest {
 
+    // @Mock
+    // private TokenServiceImpl tokenService;
+    @Mock
+    private MessageSource messageSource;
     @Mock
     private AuthenticationManager authenticationManager;
     @InjectMocks
@@ -34,21 +47,32 @@ public class CustomAuthenticationFilterTest {
     @Test
     void  Checks_For_User_If_Both_Fields_Are_Filled() throws IOException {
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.setParameter("username", "user");
-        request.setParameter("password", "user");
+        request.setMethod("POST");
+        request.setContentType("application/json");
+
+        Map<String,String> body = new HashMap<>();
+        body.put("loginInput","any");
+        body.put("password","any");
+
+        request.setContent(new ObjectMapper().writeValueAsString(body).getBytes(StandardCharsets.UTF_8));
         MockHttpServletResponse response = new MockHttpServletResponse();
+
+        when(messageSource.getMessage(any(String.class), any(), any(
+            Locale.class))).thenReturn("Some error message");
 
         Authentication result = customAuthenticationFilter.attemptAuthentication(request,response);
     }
 
-
-
     @Test
     void Throws_Exception_When_Username_Is_Null_Or_Empty() throws IOException {
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.setParameter("username", "");
+        request.setParameter("loginInput", "");
         request.setParameter("password", "user");
         MockHttpServletResponse response = new MockHttpServletResponse();
+
+        when(messageSource.getMessage(any(String.class), any(), any(
+            Locale.class))).thenReturn("Some error message");
+
 
         assertThrows(AuthenticationException.class, () ->
                 customAuthenticationFilter.attemptAuthentication(request, response));
@@ -57,9 +81,12 @@ public class CustomAuthenticationFilterTest {
     @Test
     void Throws_Exception_When_Password_Is_Null_Or_Empty() throws IOException {
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.setParameter("username", "user");
+        request.setParameter("loginInput", "user");
         request.setParameter("password", "");
         MockHttpServletResponse response = new MockHttpServletResponse();
+
+        when(messageSource.getMessage(any(String.class), any(), any(
+            Locale.class))).thenReturn("Some error message");
 
         assertThrows(AuthenticationException.class, () ->
                 customAuthenticationFilter.attemptAuthentication(request, response));
